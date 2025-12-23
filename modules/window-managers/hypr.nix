9c1128efe565dadf5cec722f607f3d0d8220e3a5
Daragh Hollman <hollmandaragh@gmail.com>
2025-12-23 13:15:42 +0000

    Add hyprland eco

--- modules/window-managers/hypr.nix
+++ modules/window-managers/hypr.nix
@@ -6,4 +6,19 @@
     xwayland.enable = true;
   };
 
+  # Screensharing
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
+  };
+
+  # Enable the rest of the ecosystem
+  environment.systemPackages = with pkgs; [
+    hyprlock
+    hypridle
+    hyprpaper
+    hyprsunset
+    hyprpicker
+  ];
+
 }
