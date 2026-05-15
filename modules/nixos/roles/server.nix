413ec2d2b1d866c5875355410a7688d52727dfff
Daragh Hollman <hollmandaragh@gmail.com>
2026-05-15 15:45:11 +0100

    Adds immich

--- modules/nixos/roles/server.nix
+++ modules/nixos/roles/server.nix
@@ -77,4 +77,13 @@ in
     enable = true;
     allowedTCPPorts = [ 80 443 ];
   };
+
+
+  # Immich
+  services.immich = {
+    enable = true;
+    port = 2283;
+    host = "0.0.0.0";
+    openFirewall = true;
+  };
 }
