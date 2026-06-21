31b2fa3f5ae831e7fe4f53ba60a504fc256d1220
Daragh Hollman <hollmandaragh@gmail.com>
2026-06-21 12:53:16 +0100

    Adds swap

--- hosts/curie/hardware-configuration.nix
+++ hosts/curie/hardware-configuration.nix
@@ -24,7 +24,10 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = 32768;
+  }];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
